201f6b83fb0e5e4dda2b5ba59d69e5ae8357a622
NgocThang-MJ <tranngocthang.bkdn@gmail.com>
2020-12-31 10:37:27 +0700

    Get email in facebook oauth

--- config/passportSetup.js
+++ config/passportSetup.js
@@ -21,11 +21,17 @@ passport.use(
       clientID: keys.FACEBOOK_APP_ID,
       clientSecret: keys.FACEBOOK_APP_SECRET,
       callbackURL: "/auth/facebook/callback",
+      profileFields: ["emails", "displayName"],
       proxy: true,
     },
     async (accessToken, refreshToken, profile, done) => {
-      console.log(profile);
-      const existingUser = await User.findOne({ id: profile.id });
+      const { email } = profile._json;
+      let existingUser = null;
+      if (email) {
+        existingUser = await User.findOne({ email });
+      } else {
+        existingUser = await User.findOne({ id: profile.id });
+      }
       if (existingUser) {
         return done(null, existingUser);
       }
@@ -33,12 +39,11 @@ passport.use(
         const newUser = await new User({
           username: profile.displayName,
           id: profile.id,
+          email: profile._json.email || "",
         });
         await newUser.save();
-        console.log("Created new user via facebook");
         done(null, newUser);
       } catch (err) {
-        console.log("Error when create user facebook oauth", err);
         return done(err);
       }
     }
